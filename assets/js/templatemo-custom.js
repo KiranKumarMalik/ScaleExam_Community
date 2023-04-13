56b8aba5e6825cfdd2f4e94692b4a7c4ae52eea5
KiranKumarMalik <malikkiran413@gmail.com>
2023-04-13 10:27:59 +0530

    changes in code

--- assets/js/templatemo-custom.js
+++ assets/js/templatemo-custom.js
@@ -134,3 +134,39 @@
 
 
 })(window.jQuery);
+
+document.onkeydown = function(e)
+    {
+        if(event.keyCode == 123)
+        {
+            return false;
+        }
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0))
+        {
+            return false;
+        }
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0))
+        {
+            return false;
+        }
+        if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0))
+        {
+            return false;
+        }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0))
+    {
+      return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
+        return false;
+     }
+     if(e.ctrlKey && e.keyCode == 'P'.charCodeAt(0)) {
+        return false;
+     }
+     if(e.ctrlKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+     }
+     if(e.ctrlKey && e.keyCode == 'V'.charCodeAt(0)) {
+        return false;
+     }
+    }
